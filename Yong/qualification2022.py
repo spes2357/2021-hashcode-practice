514853b3d0d07f71026135e6ffda908a677e0f17
Yonghwan Kim <kyongh@okstate.edu>
2022-02-24 14:50:35 -0600

    second commit

--- Yong/qualification2022.py
+++ Yong/qualification2022.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 
 a_dir = '../2021-hashcode-practice/input/a_an_example.in.txt'
+# a_dir = '../2021-hashcode-practice/input/b_better_start_small.in.txt'
 class project:
   def __init__(self, d, s, b, r):
     self.duration = d
@@ -19,8 +20,8 @@ def readInput():
       # P: projects
 	  
       C, P = list(int(t) for t in lines[0].split())
-      print("C, P")
-      print(C, P)
+      # print("C, P")
+      # print(C, P)
       # c lines: descriptions of contributors
       x = 1
       for cindex in range(1, C+1):
@@ -30,9 +31,9 @@ def readInput():
         x += 1
         for s in range(1, int(temp[1])+1):
           cTup = lines[x].split()
-          contributorDict[name].append((cTup[0], cTup[1]))
+          contributorDict[name].append((cTup[0], int(cTup[1])))
           x += 1
-      print(contributorDict)
+      # print(contributorDict)
       
       # p lines: describe projects
       for pindex in range(P):
@@ -41,11 +42,49 @@ def readInput():
         tempList = []
         for t in range(1, int(temp[4])+1):
           temp2 = lines[x].split()
-          tempList.append((temp2[0], temp2[1]))
+          tempList.append((temp2[0], int(temp2[1])))
           x += 1
         projectDict[temp[0]] = project(temp[1], temp[2], temp[3], tempList)
-        print(temp[0], projectDict[temp[0]].duration, projectDict[temp[0]].score, projectDict[temp[0]].bestbefore, projectDict[temp[0]].roles)
+        # print(temp[0], projectDict[temp[0]].duration, projectDict[temp[0]].score, projectDict[temp[0]].bestbefore, projectDict[temp[0]].roles)
+  return C, P
 
+def simulation(C, P):
+  return C
+    # tuple tester
+    # for pindex, p in enumerate(projectDict.keys()):
+    #   print(projectDict[p].roles)
+      
+    #   for x in projectDict[p].roles:
+    #     for cindex, c in enumerate(contributorDict.keys()):
+    #       for cs in contributorDict[c]:
+    #         if x[0] == cs[0]:
+    #           print(x, cs)
+    #           sdc = cs[0]
+    #           ttest = cs[1]+1
+    #           print(ttest)
+    #           contributorDict[c].remove(cs)
+    #           contributorDict[c].append((sdc, ttest))
+    #           print(contributorDict[c])
+
+
+    # ccc = 0
+    # listofproject = []
+    # for pindex, p in enumerate(projectDict.keys()):
+    #   print(pindex, p, "RQ roles: ", len(projectDict[p].roles))
+    #   counter = 0
+    #   for cindex, c in enumerate(contributorDict.keys()):
+    #     for r in projectDict[p].roles:
+    #       for skill in contributorDict[c]:
+    #         if r[0] == skill[0] and int(r[1]) <= int(skill[1]):
+    #           counter += 1
+    #           # print("Project roles: ", r, c, contributorDict[c])
+    #   print("Role filled: " ,counter)
+    #   if len(projectDict[p].roles) == counter:
+    #     ccc += 1
+    #     listofproject.append(p)
+    # print(len(projectDict.keys()), ccc)
+    # print(listofproject)
+        
 if __name__=="__main__":
-    readInput()
-    # simulation()
+    C, P = readInput()
+    simulation(C, P)
